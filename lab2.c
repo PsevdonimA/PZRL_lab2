6ad54f41b371825198f5ca4bab4bc06e1562c028
PsevdonimA <mgame3341@gmail.com>
2024-12-08 21:35:50 +0300

    lab2.h lab2.c added; func checkArgv() added

---
+++ lab2.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "lab2.h"
+
+int checkArgv(int argc, char** argv) // 0 - correct; other numbers - errors
+{
+    if (argc != 3)
+    {
+        return 1; // wrong aruments number
+    }
+    if (access(argv[1], F_OK) != 0)
+    {
+        return 2; // cannot find selected file
+    }
+    return 0;
+}
