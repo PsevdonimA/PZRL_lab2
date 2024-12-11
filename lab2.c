fdc7f3e8f7e2181d7e3a751aa36ff19da6a71222
PsevdonimA <mgame3341@gmail.com>
2024-12-11 20:18:03 +0300

    func changeText() added

--- lab2.c
+++ lab2.c
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <regex.h>
+#include <string.h>
 #include "lab2.h"
 
 int checkArgv(int argc, char** argv) // 0 - correct; other numbers - errors
@@ -12,4 +14,74 @@ int checkArgv(int argc, char** argv) // 0 - correct; other numbers - errors
         return 2; // cannot find selected file
     }
     return 0;
+}
+
+int changeText(char** ptrs, int n, char* str)
+{
+    if (str == NULL || ptrs == NULL)
+    {
+        return 1; // NULL pointer
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (ptrs[i] == NULL)
+        {
+            return 1; // NULL pointer
+        }
+    }
+    regex_t regex;
+    // Replace
+    int regnok = regcomp(&regex, "^s\/[^\/][^\/]*\/[^\/]*\/$", 0);
+    if (regnok)
+    {
+        regfree(&regex);
+        return 2; // Reg compile error
+    }
+    regnok = regexec(&regex, str, 0, NULL, 0);
+    if (!regnok)
+    {
+        regfree(&regex);
+        return 0;
+    }
+    // Delete
+    regnok = regcomp(&regex, "^\/[^\/][^\/]*\/d$", 0);
+    if (regnok)
+    {
+        regfree(&regex);
+        return 2; // Reg compile error
+    }
+    regnok = regexec(&regex, str, 0, NULL, 0);
+    if (!regnok)
+    {
+        regfree(&regex);
+        return 0;
+    }
+    // Insert front
+    regnok = regcomp(&regex, "^s\/\^\/[^\/][^\/]*\/$", 0);
+    if (regnok)
+    {
+        regfree(&regex);
+        return 2; // Reg compile error
+    }
+    regnok = regexec(&regex, str, 0, NULL, 0);
+    if (!regnok)
+    {
+        regfree(&regex);
+        return 0;
+    }
+    // Insert back
+    regnok = regcomp(&regex, "^s\/\$\/[^\/][^\/]*\/$", 0);
+    if (regnok)
+    {
+        regfree(&regex);
+        return 2; // Reg compile error
+    }
+    regnok = regexec(&regex, str, 0, NULL, 0);
+    if (!regnok)
+    {
+        regfree(&regex);
+        return 0;
+    }
+    regfree(&regex);
+    return 3; // No match
 }
--- lab2.h
+++ lab2.h
@@ -2,5 +2,6 @@
 #define LAB2_H
 
 int checkArgv(int argc, char** argv);
+int changeText(char** ptrs, int n, char* str);
 
 #endif
